239c8a0a64afbd04378ed2777a587576de612c3a
Nishan9t <pandeynisha9t@gmail.com>
2023-09-10 12:35:50 +0530

    contact about and courses page'f frontend is done

--- client/src/components/Courses.jsx
+++ client/src/components/Courses.jsx
@@ -2,6 +2,32 @@ import React from 'react'
 
 export default function Courses() {
   return (
-    <div>Courses</div>
+    <div className='lg:flex-wrap md:flex lg:mx-auto'>
+   
+      <div className='flex border border-2 border-solid p-2 ml-8 my-4 h-32 lg:w-3/4 sm:w-full md:w-full shadow-lg text-center rounded-md'>
+            <div className='font-bold text-lg capitalize'>course name</div>
+            <div className='m-4'>course decription</div>
+      </div>
+      <div className='flex border border-2 border-solid p-2 ml-8 my-4 h-32 lg:w-3/4 sm:w-full md:w-full shadow-lg text-center rounded-md'>
+            <div className='font-bold text-lg capitalize'>course name</div>
+            <div className='m-4'>course decription</div>
+      </div>
+      <div className='flex border border-2 border-solid p-2 ml-8 my-4 h-32 lg:w-3/4 sm:w-full md:w-full shadow-lg text-center rounded-md'>
+            <div className='font-bold text-lg capitalize'>course name</div>
+            <div className='m-4'>course decription</div>
+      </div>
+      <div className='flex border border-2 border-solid p-2 ml-8 my-4 h-32 lg:w-3/4 sm:w-full md:w-full shadow-lg text-center rounded-md'>
+            <div className='font-bold text-lg capitalize'>course name</div>
+            <div className='m-4'>course decription</div>
+      </div>
+      <div className='flex border border-2 border-solid p-2 ml-8 my-4 h-32 lg:w-3/4 sm:w-full md:w-full shadow-lg text-center rounded-md'>
+            <div className='font-bold text-lg capitalize'>course name</div>
+            <div className='m-4'>course decription</div>
+      </div>
+      <div className='flex border border-2 border-solid p-2 ml-8 my-4 h-32 lg:w-3/4 sm:w-full md:w-full shadow-lg text-center rounded-md'>
+            <div className='font-bold text-lg capitalize'>course name</div>
+            <div className='m-4'>course decription</div>
+      </div>
+    </div>
   )
 }
--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -17,12 +17,12 @@ useEffect(()=>{
 })
 
   return (
-    <div className='lg:flex md:flex lg:mr-4'>
+    <div className='lg:flex-wrap md:flex '>
     {
       data.length > 0? 
       data.map((serviceItem)=>{
         return(
-          <div key={serviceItem._id} className='border border-2 border-solid p-2 mt-4 ml-4 h-80 lg:w-1/4 sm:w-full md:w-full shadow-lg text-center rounded-md'>
+          <div key={serviceItem._id} className='border border-2 border-solid p-2 mx-8 my-4 h-80 lg:w-80 sm:w-full md:w-full shadow-lg text-center rounded-md'>
             <div className='font-bold text-lg capitalize'>{serviceItem.title}</div>
             <div className=''>{serviceItem.description}</div>
           </div>
