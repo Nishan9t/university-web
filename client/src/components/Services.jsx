3f7dd7c5534ca65ae305732e2f0d8ea7db8cc41f
Nishan9t <pandeynisha9t@gmail.com>
2023-09-10 11:50:08 +0530

    service cards is complete and some responsiveness is added in card and navbar

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -17,14 +17,14 @@ useEffect(()=>{
 })
 
   return (
-    <div>
+    <div className='lg:flex md:flex lg:mr-4'>
     {
       data.length > 0? 
       data.map((serviceItem)=>{
         return(
-          <div key={serviceItem._id}>
-            <div>{serviceItem.title}</div>
-            <div>{serviceItem.description}</div>
+          <div key={serviceItem._id} className='border border-2 border-solid p-2 mt-4 ml-4 h-80 lg:w-1/4 sm:w-full md:w-full shadow-lg text-center rounded-md'>
+            <div className='font-bold text-lg capitalize'>{serviceItem.title}</div>
+            <div className=''>{serviceItem.description}</div>
           </div>
         )
       })
