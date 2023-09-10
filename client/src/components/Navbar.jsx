4fb1e06b207fcff580f07c92f41ae4a75d5297b9
Nishan9t <pandeynisha9t@gmail.com>
2023-09-10 11:15:48 +0530

    navbar styling fixed and services data is showing

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,19 +3,19 @@ import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <div className='flex flex-row justify-between bg-green-400 p-4' >
-        <div>
+    <div className='flex flex-row justify-between bg-green-400 px-4' >
+        <div className='px-2 py-4'>
             Univeristy
         </div>
 
         {/* Links */}
         <div className='flex flex-row'>
 
-            <div className='pl-4 cursor-pointer hover:text-red-500'> <Link to="/home">Home</Link>        </div>
-            <div className='pl-4 cursor-pointer hover:text-red-500'> <Link to="/courses">Courses</Link>  </div>
-            <div className='pl-4 cursor-pointer hover:text-red-500'> <Link to="/about">About</Link>       </div>
-            <div className='pl-4 cursor-pointer hover:text-red-500'> <Link to="/services">Services</Link> </div>
-            <div className='pl-4 cursor-pointer hover:text-red-500'> <Link to="/contact">Contact</Link>    </div>
+            <div className='px-2 py-4 hover:pb-0 font-bold cursor-pointer hover:text-red-500 hover:border-b-4 hover:border-red-500'> <Link to="/home">Home</Link>        </div>
+            <div className='px-2 py-4 hover:pb-0 font-bold cursor-pointer hover:text-red-500 hover:border-b-4 hover:border-red-500'> <Link to="/courses">Courses</Link>  </div>
+            <div className='px-2 py-4 hover:pb-0 font-bold cursor-pointer hover:text-red-500 hover:border-b-4 hover:border-red-500'> <Link to="/about">About</Link>       </div>
+            <div className='px-2 py-4 hover:pb-0 font-bold cursor-pointer hover:text-red-500 hover:border-b-4 hover:border-red-500'> <Link to="/services">Services</Link> </div>
+            <div className='px-2 py-4 hover:pb-0 font-bold cursor-pointer hover:text-red-500 hover:border-b-4 hover:border-red-500'> <Link to="/contact">Contact</Link>    </div>
 
         </div>
     </div>
--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 export default function Services() {
+
+  const [data,setData]=useState([])
+
+useEffect(()=>{
+  axios.get('http://localhost:8000/api/services')
+  .then(res=>{
+    console.log(res.data)
+    setData(res.data.data)
+  })
+  .catch(err=>{
+    console.log(err);
+  })
+})
+
   return (
-    <div>Services</div>
+    <div>
+    {
+      data.length > 0? 
+      data.map((serviceItem)=>{
+        return(
+          <div key={serviceItem._id}>
+            <div>{serviceItem.title}</div>
+            <div>{serviceItem.description}</div>
+          </div>
+        )
+      })
+      :'no data found'
+    }
+
+    </div>
   )
 }
