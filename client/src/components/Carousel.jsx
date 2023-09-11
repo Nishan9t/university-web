aff3405dec6ba23fb778d831823248ff0c33b94f
Nishan9t <pandeynisha9t@gmail.com>
2023-09-11 12:22:55 +0530

    carousel button is added

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -1,9 +1,35 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import {FaLessThan,FaGreaterThan} from 'react-icons/fa'
 
 export default function Carousel() {
 
     const [images,setImages] = useState([])
+    const [index,setIndex]=useState(0);
+
+    function indexSet(work){
+      if(index===2 && work==="+")
+      {
+        setIndex(0);
+      }
+      else if(index===0 && work==="-")
+      {
+        setIndex(2);
+      }
+      else{
+        if(work==="+")
+        {
+          setIndex((prev)=> prev+1);
+        }
+        else 
+        {
+          setIndex((prev)=> prev-1);
+        }
+        
+      }
+      }
+
+    
 
     useEffect(()=>{
         axios.get('http://localhost:8000/api/slider')
@@ -15,8 +41,13 @@ export default function Carousel() {
             console.log(err)
         })
 
-    },[])
+    },[index])
   return (
-    <div>Carousel</div>
+    <div className='flex '>
+            <button onClick={()=>indexSet("-")}><FaLessThan/></button>
+            <img className='' src={images[index]}alt="carousel"/>{index}
+            <button onClick={()=>indexSet("+")}><FaGreaterThan/></button>
+            
+    </div>
   )
 }
