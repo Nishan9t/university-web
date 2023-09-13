97d6a0cc8c6b4b05a194f97e2346dce5714865cd
Nishan9t <pandeynisha9t@gmail.com>
2023-09-13 11:26:36 +0530

    filter , sorting and searching is done in services page

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -4,7 +4,8 @@ import Footer from './Footer'
 
 export default function Services() {
 
-  const [data,setData]=useState([])
+  const [data,setData]=useState([]);
+  const [filter,setFilter]= useState('')
 
 useEffect(()=>{
   axios.get('http://localhost:8000/api/services')
@@ -18,11 +19,20 @@ useEffect(()=>{
 })
 
   return (
-  
-    <div className='lg:flex-wrap md:flex mb-36 justify-center'>
+  <div className='mb-36'>
+        <div className='flex justify-end items-center'>
+        
+        <input type="text" className='border border-2 p-1 m-2 border-black w-80 rounded-sm' placeholder='Title' value={filter} onChange={(e)=>setFilter(e.target.value)}></input>
+        </div>
+    <div className='lg:flex-wrap md:flex  justify-center'>
     {
       data.length > 0? 
-      data.map((serviceItem)=>{
+      data
+      .sort((a,b)=>a.title.toLowerCase() > b.title.toLowerCase()? 1: -1)
+      .filter((item)=>{
+        return item.title.toLowerCase().includes(filter.toLocaleLowerCase())
+      })
+      .map((serviceItem)=>{
         return(
           <div key={serviceItem._id} className=' border-2 border-solid p-2  mx-8 my-4 h-80 lg:w-1/4 sm:w-full md:w-full shadow-lg rounded-md overflow-hidden'>
             <div className='font-bold text-lg capitalize mb-2 underline'>{serviceItem.title}</div>
@@ -33,6 +43,7 @@ useEffect(()=>{
       :'no data found'
     }
 
+    </div>
     </div>
 
   
