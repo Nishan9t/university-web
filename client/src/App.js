0a0f7cdbb1d524f5e175026835f86a67bffd9bcc
Nishan9t <pandeynisha9t@gmail.com>
2023-09-19 12:13:31 +0530

    dropdown for adding contents is made

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,7 @@ function App() {
     <BrowserRouter>
     <Navbar/>
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/courses" element={<Courses />} />
         <Route path="/services" element={<Services />} />
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 
 export default function Navbar() {
@@ -6,9 +6,28 @@ export default function Navbar() {
   const navigate=useNavigate()
   const type=localStorage.getItem('type')
   const token=localStorage.getItem('token')
+
+  const [drop , setDrop ]=useState('')
+
+useEffect(()=>{
+  if(drop==='course')
+  {
+    navigate('/admin/courses')
+  }
+  else if(drop==='service')
+  {
+    navigate('/admin/services')
+  }
+},[drop])
+
   useEffect(()=>{
+   
 
   },[type,token])
+
+
+
+
   return (
     <div className='lg:flex justify-between bg-green-400 px-4' >
         <div className='px-2 lg:py-4'>
@@ -18,19 +37,30 @@ export default function Navbar() {
         {/* Links */}
         <div className='flex flex-row'>
 
-            <div className='px-2 lg:py-4 hover:pb-0 font-bold cursor-pointer hover:text-red-500 hover:border-b-4 hover:border-red-500'> <Link to="/home">Home</Link>        </div>
+            <div className='px-2 lg:py-4 hover:pb-0 font-bold cursor-pointer hover:text-red-500 hover:border-b-4 hover:border-red-500'> <Link to="/">Home</Link>        </div>
             <div className='px-2 lg:py-4 hover:pb-0 font-bold cursor-pointer hover:text-red-500 hover:border-b-4 hover:border-red-500'> <Link to="/courses">Courses</Link>  </div>
             <div className='px-2 lg:py-4 hover:pb-0 font-bold cursor-pointer hover:text-red-500 hover:border-b-4 hover:border-red-500'> <Link to="/about">About</Link>       </div>
             <div className='px-2 lg:py-4 hover:pb-0 font-bold cursor-pointer hover:text-red-500 hover:border-b-4 hover:border-red-500'> <Link to="/services">Services</Link> </div>
             <div className='px-2 lg:py-4 hover:pb-0 font-bold cursor-pointer hover:text-red-500 hover:border-b-4 hover:border-red-500'> <Link to="/contact">Contact</Link>    </div>
             {
               localStorage.getItem('token') &&
+              <div className='lg:py-4 hover:pb-0 font-bold cursor-pointer border-none hover:text-red-500'> 
+              <select value={drop} onChange={(e)=>setDrop(e.target.value)} className='hover:text-red-500  border-none bg-green-400 text-center p-0 active:border-none focus:border-none' >
+                <option>Add Contents</option>
+                <option className='border-none active:border-0 hover:border-0 focus:border-none' value={'service'} >Add Services</option>
+                <option className='border-none active:border-0 hover:border-0 focus:border-none' value={'course'}>Add Courses</option>
+              </select>
+              
+              </div>
+            }
+            {/* {
+              localStorage.getItem('token') &&
               <div className='px-2 lg:py-4 hover:pb-0 font-bold cursor-pointer hover:text-red-500 hover:border-b-4 hover:border-red-500'> <Link to="/admin/services">Add Services</Link>    </div>
             }
             {
               localStorage.getItem('token') &&
               <div className='px-2 lg:py-4 hover:pb-0 font-bold cursor-pointer hover:text-red-500 hover:border-b-4 hover:border-red-500'> <Link to="/admin/courses">Add Courses</Link>    </div>
-            }
+            } */}
             {
               localStorage.getItem('token') && localStorage.getItem('type')==='ADMIN'?
               <div className='px-2 lg:py-4 hover:pb-0 font-bold cursor-pointer hover:text-red-500 hover:border-b-4 hover:border-red-500'> <Link to="/admin/list">Admin List</Link>    </div>
